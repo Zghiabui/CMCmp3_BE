6dab26e52ea13436edb69e1265a2df30c17254dd
Zghiabui <buitrongnghiaaaaa.com>
2025-11-04 14:03:40 +0700

    first commit

---
+++ src/main/java/com/example/CMCmp3/CmCmp3Application.java
@@ -0,0 +1,13 @@
+package com.example.CMCmp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmCmp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmCmp3Application.class, args);
+	}
+
+}
