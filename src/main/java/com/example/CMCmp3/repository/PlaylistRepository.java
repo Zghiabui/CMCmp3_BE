3796a074a524624693cdd1f6ee2f0037fea23adb
nkminh14 <ngyenkhoaminh0104@gmail.com>
2025-11-07 15:29:20 +0700

    tạo playlist

---
+++ src/main/java/com/example/CMCmp3/repository/PlaylistRepository.java
@@ -0,0 +1,12 @@
+package com.example.CMCmp3.repository;
+
+import com.example.CMCmp3.entity.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+    List<Playlist> findByUserId(Long userId);
+}
