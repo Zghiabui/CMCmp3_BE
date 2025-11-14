2efc41b28667f1392f63a5c347b1379a7150c09f
TagiaHuy <tagiahuy2605@gmail.com>
2025-11-14 22:50:50 +0700

    v1

--- src/main/java/com/example/CMCmp3/dto/PlaylistDTO.java
+++ src/main/java/com/example/CMCmp3/dto/PlaylistDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -13,6 +14,7 @@ public class PlaylistDTO {
     private String title;
     private String description;
     private String imageUrl;
+    private List<Long> songs;
     private Long playCount;
     private Long likeCount;
     private Long commentCount;
